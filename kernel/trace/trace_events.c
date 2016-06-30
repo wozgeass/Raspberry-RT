6b61e876a132c14d34e26f2ad5dfba743ff1ea38
wozgeass <adrian@aztli.org>
2016-06-29 21:18:40 -0500

    Actualizados 8 archivos de la carpeta trace

--- kernel/trace/trace_events.c
+++ kernel/trace/trace_events.c
@@ -188,8 +188,6 @@ static int trace_define_common_fields(void)
 	__common_field(unsigned char, flags);
 	__common_field(unsigned char, preempt_count);
 	__common_field(int, pid);
-	__common_field(unsigned short, migrate_disable);
-	__common_field(unsigned short, padding);
 
 	return ret;
 }
@@ -245,7 +243,7 @@ void *trace_event_buffer_reserve(struct trace_event_buffer *fbuffer,
 		return NULL;
 
 	local_save_flags(fbuffer->flags);
-	fbuffer->pc = event_preempt_count();
+	fbuffer->pc = preempt_count();
 	fbuffer->trace_file = trace_file;
 
 	fbuffer->event =
